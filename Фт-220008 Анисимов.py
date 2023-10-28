810ac26f869e2e3b47c6722b365ad5d15a701e12
Vancheres <anisii840@gmail.com>
2023-10-29 00:30:03 +0500

    vesa

--- Фт-220008 Анисимов.py
+++ Фт-220008 Анисимов.py
@@ -1,8 +1,27 @@
 def main():
     n=int(input("Введите количество критериев"))
-    a=[[]]
+    a=[[0]*n for i in range(n)]
     for i in range(n):
-        for j in range(n)
+        for j in range(n):
+            if (i == j):
+                a[i][j] = 1.00
+            else:
+                print("Введите отношение критерия", i+1 , "к критерию", j+1,"(число в десятичном формате)")
+                a[i][j]=float(input())
+    ves(a)
+
+def ves(a,n):
+    sum =0.0
+
+    for i in range(n):
+        for j in range(n):
+            sum+=a[i][j]
+
+
+
+
+
+
 
 if __name__ == "__main__":
         main()
